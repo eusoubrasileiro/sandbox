c2c277655da61b90625a789acaabf5d546b20a28
eusoubrasileiro <eusoubrasileiro@gmail.com>
2022-09-15 22:18:44 -0300

    playing with camera and child sleeping patterns

--- loose/night_pattern.py
+++ loose/night_pattern.py
@@ -5,7 +5,7 @@ from matplotlib import pylab as plt
 import datetime
 
 
-def plot_nights(df, body_pixels, cam_name='frontwall', verbose=False, nights=1,
+def plot_nights(df, body_pixels, cam_name='frontwall', verbose=False, nnights=1,
                     stacking='selected', addtext=''):
     """
         Assumptions: a. nothing besides the child is moving inside the room.  
@@ -27,7 +27,9 @@ def plot_nights(df, body_pixels, cam_name='frontwall', verbose=False, nights=1,
     if verbose:
         plt.figure()
         df.span.apply(lambda x: x/60.).hist(bins=40)
-    dfp = df.resample('5T').sum() # resample 5 min, summing where there was no data it uses 0    
+    
+    resample = 3 # resample factor
+    dfp = df.resample(f'{resample}T').sum() # resample 3 min, summing where there was no data it uses 0        
     dance_pixels = 2*body_pixels
     #dfp['nchange_pixels'] += 1 # to avoid log of 0
     dfp['nchange_pixels'] = 100*dfp['nchange_pixels']/dance_pixels # just nromalize by max movment    
@@ -50,55 +52,66 @@ def plot_nights(df, body_pixels, cam_name='frontwall', verbose=False, nights=1,
     if verbose: # stair-step night code plot
         plt.figure()
         dfp['night'].map(lambda x: int(x.month*100 + x.day)).plot()
-    series =  []    
+    nights =  []    
     msgs = [] 
-    params = []
-    for night, night_data in dfp.groupby(dfp.night)['nchange_pixels']:     
-        sp = night_data[ ( (night_data.index.time > datetime.time(hour=20)) & (night_data.index.time < datetime.time(hour=23, minute=59, second=59)) ) | 
-            ( (night_data.index.time > datetime.time(hour=0)) & (night_data.index.time < datetime.time(hour=6)) ) ]
-        if len(sp) == 0 or len(sp) != 118:
-            print(f"ignored night data { night } len {len(sp)}")
+    nights_info = []
+    # from 20:00 to 6:00 are 10 hours - 
+    nsamples = (60//resample)*10 # number of samples per night 
+    for night, dfnight in dfp.groupby(dfp.night)['nchange_pixels']:     
+        night_data = dfnight[ ( (dfnight.index.time >= datetime.time(hour=20)) & (dfnight.index.time < datetime.time(hour=23, minute=59, second=59)) ) | 
+            ( (dfnight.index.time > datetime.time(hour=0)) & (dfnight.index.time <= datetime.time(hour=6)) ) ]
+        if len(night_data) != nsamples:
+            print(f"ignored night data { night } len {len(night_data)}")
             continue 
-        series.append(sp)
-        params.append([night.strftime("%m/%d"), sp.index.time[0], sp.index.time[-1], 
-            np.sum(sp.values), np.count_nonzero(sp.values), sp.size])
-        msgs.append(f"day {params[-1][0]} time {params[-1][1]} {params[-1][2]} sum_movs {params[-1][3]:5.1f} event count {params[-1][4]:3d} {params[-1][5]}")
+        nights.append(night_data)
+        nights_info.append([night.strftime("%m/%d"), night_data.index.time[0], night_data.index.time[-1], 
+            np.sum(night_data.values), np.count_nonzero(night_data.values), night_data.size])
+        msgs.append(f"day {nights_info[-1][0]} time {nights_info[-1][1]} {nights_info[-1][2]} sum_movs {nights_info[-1][3]:5.1f} event count {nights_info[-1][4]:3d} {nights_info[-1][5]}")
         if verbose:
             print(msgs[-1])    
-    # plot number of last nights defined
-    fig, axis = plt.subplots(nights,1,figsize=(15,4*nights)) 
-    last_nights = series[-nights:]     # latests nights 
-    nights = [ param[0] for param in params[-nights:]]
-    for i, sp in enumerate(last_nights):        
-        axis[i].plot(sp.index.values, sp.values, linewidth=0.5, color='b', label='night of day '+nights[i]+' '+addtext)                             
-        axis[i].fill_between(sp.index, sp.values, 0)
+    # plot number of last nights defined    
+    ngraphs = nnights+1 if stacking else nnights    
+    fig, axis = plt.subplots(ngraphs,1,figsize=(15,4*ngraphs)) 
+    last_nights = nights[-nnights:]     # latests nights 
+    nights_info = [ param[0] for param in nights_info[-nnights:]]
+    for i, night_data in enumerate(last_nights):        
+        axis[i].plot(night_data.index.values, night_data.values, linewidth=0.3, color='b', label='night of day '+nights_info[i]+' '+addtext)                      
+        axis[i].fill_between(night_data.index, night_data.values, 0, color='gray', alpha=0.5)
+        smoothed = night_data.rolling(window=5).mean().values
+        axis[i].plot(night_data.index.values, smoothed, linewidth=0.5, color='r', alpha=0.5)       
+        axis[i].fill_between(night_data.index, smoothed, 0)
         axis[i].grid()
         axis[i].set_ylim(0, 100)        
         axis[i].set_ylabel('% Dancing Kid')  
-        # We need to draw the canvas, otherwise the labels won't be positioned and 
-        # won't have values yet.
+        # We need to draw the canvas, otherwise the labels won't be positioned and - won't have values yet.
         fig.canvas.draw()
         # remove date from tick labels
         ticks = [ t.get_text().split()[-1] for t in axis[i].get_xticklabels() ]
         axis[i].set_xticklabels(ticks)
-        axis[i].legend(fontsize=18)
+        axis[i].legend(fontsize=18)    
     if stacking: # stack to see any underline pattern        
+        i=i+1 # last graph 
         if 'selected' in stacking:
-            series = last_nights 
-        s0 = series[0].values.copy() 
-        for night_data in series: # series[2:]:
-            s0 += night_data.values
-        fig, axis = plt.subplots(1,1,figsize=(15,4)) 
-        s0 = s0/(len(series)-1)
-        axis.plot(night_data.index, s0, color='b', linewidth=0.5, label='stacked nights')
-        axis.set_ylim(min(s0), max(s0))
-        axis.fill_between(night_data.index, s0, 0, color='k')
-        axis.legend(fontsize=18)
+            nights = last_nights         
+        sum = nights[0].values 
+        for night in nights[1:]:  # stack = summing 
+            sum += night.values
+        sum = sum/len(nights)
+        axis[i].plot(night.index, sum, color='b', linewidth=0.3, label='stacked nights')
+        axis[i].set_ylim(min(sum), max(sum))
+        axis[i].fill_between(night.index, sum, 0, color='gray', alpha=0.5)
+        def moving_average(x, w):
+            return np.append(np.zeros(w-1), np.convolve(x, np.ones(w), 'valid') / w)
+        smoothed = moving_average(sum, 5)
+        axis[i].plot(night.index, smoothed, linewidth=0.5, color='r', alpha=0.5)       
+        axis[i].fill_between(night.index, smoothed, 0)
+        axis[i].grid()
+        axis[i].legend(fontsize=18)
         fig.canvas.draw()
         # remove date from tick labels
-        ticks = [ t.get_text().split()[-1] for t in axis.get_xticklabels() ]
-        axis.set_xticklabels(ticks)
-        print("stacked ", len(series), " series")
+        ticks = [ t.get_text().split()[-1] for t in axis[i].get_xticklabels() ]
+        axis[i].set_xticklabels(ticks)
+        print(f" stacked {len(nights)} series")
     return dfp 
 
 
